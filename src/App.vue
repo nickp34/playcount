f666a35f6757d9675abb9798934e17ae8411f73b
Nick Pate <saleen34@gmail.com>
2022-09-24 10:54:38 -0700

    save

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,20 @@
 <template>
   <div>
     <v-app>
-      <!-- <v-app-bar
-        app
-        color="primary"
-        dark
-      >
-        <div class="d-flex align-center">
-          <v-btn @click="undoPlay">
-            <span>Undo</span>
-          </v-btn>
-          <v-btn :color="savePlayButtonColor" @click="savePlay">
-            Save
-          </v-btn>
-          <v-btn :color="savePlayButtonColor">
-            {{ playerInCount }}
-          </v-btn>
-          <v-btn color="orange" @click="clearField">
-            Clear
-          </v-btn>
-        </div>
-      </v-app-bar> -->
-
       <v-main>
         <v-tabs>
           <v-tab>Game</v-tab>
           <v-tab>Stats</v-tab>
+          <!-- <v-tab>Roster</v-tab> -->
           <v-tab-item>
             <FootballGame />
           </v-tab-item>
           <v-tab-item>
             <FootballPlayerStats />
           </v-tab-item>
+          <!-- <v-tab-item>
+            <FootballRoster />
+          </v-tab-item> -->
         </v-tabs>
         <v-alert v-if="showSuccess" type="success">Success</v-alert>
       </v-main>
@@ -43,6 +26,7 @@
 import { mapGetters } from 'vuex';
 import FootballGame from './components/FootballGame.vue';
 import FootballPlayerStats from './components/FootballPlayerStats.vue';
+// import FootballRoster from './components/FootballRoster.vue';
 
 export default {
   name: 'App',
@@ -50,30 +34,17 @@ export default {
     ...mapGetters({
       showSuccess: 'showSuccess',
     }),
-    // playerInCount() {
-    //   return this.playersIn.length;
-    // },
-    // savePlayButtonColor() {
-    //   if (this.playerInCount < 5 || this.playerInCount > this.getMaxPlayers) {
-    //     return 'red';
-    //   }
-    //   return this.playerInCount < this.getMaxPlayers ? 'orange' : 'green';
-    // },
   },
-  // methods: {
-  //   savePlay() {
-  //     this.$store.dispatch('savePlay');
-  //   },
-  //   undoPlay() {
-  //     this.$store.dispatch('undoPlay');
-  //   },
-  //   clearField() {
-  //     this.$store.dispatch('clearField');
-  //   },
-  // },
   components: {
     FootballGame,
     FootballPlayerStats,
+    // FootballRoster,
   },
 };
 </script>
+
+<style>
+.body {
+  overscroll-behavior: contain;
+}
+</style>
--- src/components/FootballRoster.vue
+++ src/components/FootballRoster.vue
@@ -2,16 +2,14 @@
   <v-container v-bind:style="{ 'background-color': '#e3e3e3' }">
     <v-row>
       <v-col>
-        <v-btn color="blue" @click="toggleNames">
-          Names
+        <v-btn color="blue">
+          Save
         </v-btn>
       </v-col>
     </v-row>
     <v-row>
       <v-col v-for="player in sortedRoster" :key="player.jersey" cols="12" sm="4">
         <FootballPlayer
-          showPlayCount
-          :showName="showName"
           :player=player
         />
       </v-col>
@@ -24,7 +22,7 @@ import { mapGetters } from 'vuex';
 import FootballPlayer from './FootballPlayer.vue';
 
 export default {
-  name: 'FootballPlayerStats',
+  name: 'FootballRoster',
   data: () => ({
     showName: true,
   }),
@@ -37,9 +35,6 @@ export default {
     },
   },
   methods: {
-    toggleNames() {
-      this.showName = !this.showName;
-    },
   },
   components: {
     FootballPlayer,
